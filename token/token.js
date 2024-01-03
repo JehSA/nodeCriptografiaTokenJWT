5b21ac9b051e3df36dc618169d57d5aa16c1ffa1
JehSA <jef.almeida@gmail.com>
2024-01-02 21:15:42 -0300

    token JWT

---
+++ token/token.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+
+const chaveSecreta = "Chave super secreta";
+
+const token = jwt.sign(
+    {
+        apelido: "jm",
+        curso: "segurança e node.js"
+    }, 
+    chaveSecreta
+);
+
+console.log(token);
+
+
+const tokenDecodificado = jwt.verify(token, chaveSecreta);
+console.log(tokenDecodificado);
