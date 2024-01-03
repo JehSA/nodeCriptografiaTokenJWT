fada6b258fd965df4c1433e411ba2cde237438e2
JehSA <jef.almeida@gmail.com>
2024-01-03 09:50:21 -0300

    Simulacoes de ataques(forca bruta, dicionario e rainbow table).

---
+++ simulacaoAtaques/forcaBruta.js
@@ -0,0 +1,33 @@
+import { createHash } from 'crypto';
+
+class Usuario {
+
+    constructor(nome, senha) {
+        this.nome = nome;
+        this.hash = this.criaHash(senha);
+    }
+
+    criaHash(senha) {
+        return createHash('sha256').update(senha).digest('hex');
+    }
+
+    autentica(nome, senha) {
+        if(nome === this.nome && this.hash === this.criaHash(senha)) {
+            console.log("Usuário autenticado com sucesso!");
+            return true;
+        }
+        //console.log("Usuário ou senha incorretos.");
+        return false;
+    }
+
+}
+
+const usuario = new Usuario('Jeh', '1337');
+
+
+// Ataque
+for(let senhaTeste = 0; senhaTeste < 10000; senhaTeste++) {
+    if(usuario.autentica("Jeh", senhaTeste.toString())) {
+        console.log(`A senha do usuário é: ${senhaTeste}`);
+    }
+}
